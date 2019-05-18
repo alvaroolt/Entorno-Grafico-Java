ef87be5bcd19b13f1d50b28cdfe60e5fe98f28ab
Álvaro <36482489+alvaroolt@users.noreply.github.com>
2019-05-18 13:47:14 +0200

    Delete TestVentana.java

--- src/entornoGraficoEjemplos/TestVentana.java
+++
@@ -1,9 +0,0 @@
-package entornoGraficoEjemplos;
-
-public class TestVentana {
-  public static void main(String[] args) {
-    Ventana1 ventana = new Ventana1();
-    BotonConvertir evento = new BotonConvertir(null);
-    ventana.setVisible(true);
-  }
-}
